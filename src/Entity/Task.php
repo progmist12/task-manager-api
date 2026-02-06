d608cd061ff02cad2d36844f192c8c9cbe2a7d6a
lance <lancevera8@gmail.com>
2026-02-06 14:07:29 +0800

    Added changes

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -6,6 +6,23 @@ use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Delete()
+    ]
+)]
+
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 class Task
 {
